2c524a51cd5dce117559a46f0bd543ddb42782bc
Ray <farny100006306826831@gmail.com>
2025-03-26 00:13:47 +0800

    new commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
       return [
         {
           source: "/api/chat", // 前端請求的路徑
-          destination: "http://192.168.1.107:8000/chat", // 轉發到後端 FastAPI
+          destination: "http://125.224.96.183:8000/chat", // 轉發到後端 FastAPI
         },
       ];
     },
